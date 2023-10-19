0da0eeb0858b061722509cb477c48fa2bf31e508
Jonnathan <jonathan.ferrucho@gmail.com>
2023-10-19 16:56:51 -0500

    test

--- ci/build-lambda.py
+++ ci/build-lambda.py
@@ -29,7 +29,7 @@ async def main():
             # )
             .with_exec(["zip", "-r", "./function.zip", "."])
             .with_workdir("/lambda")
-            .with_exec(["zip", "function.zip", "handler.py"])
+            # .with_exec(["zip", "function.zip", "handler.py"])
         )
 
         export = (
